e7cb79744de1d6cdccffddb2c4c79b6bc03e144f
negi18kartik <negi18kartik@gmail.com>
2024-03-07 23:36:07 +0530

    Login & Signup Page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import SignUp from "../templates/SignInPage/SignUp/signUp";
+import Login from "../templates/SignInPage/Login/login";
+import Main from "../components/LeftSideBar/main";
 
 const App = () => {
   return (
-    <div>
-      <SignUp />
+    <div className="bg-neutral-900">
+      {/* <SignUp /> */}
+      {/* <Login /> */}
+      <Main />
     </div>
   );
 };
--- templates/SignInPage/Login/login.jsx
+++ templates/SignInPage/Login/login.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import { FaGoogle } from "react-icons/fa6";
 
-const SignUp = () => {
-  const [username, setUsername] = useState("");
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted:", { username, email, password });
+    console.log("Submitted:", { email, password });
   };
 
   return (
     <div className="h-screen flex max-md:flex-col">
-      <div className="h-1/3 sign-up-background min-lg:hidden" />
+      <div className="h-1/3 login-background min-lg:hidden" />
+      <div className="h-screen w-screen login-background max-md:hidden" />
       <div className="h-full w-screen flex flex-col">
         <span className="px-4 pt-4 text-end flex flex-col max-md:pt-2">
           <p className="text-xs text-slate-500 font-medium tracking-wide">
-            Already An Existing User?
+            Still Haven't Logged In Yet?
           </p>
           <p className="mr-1 text-lg font-bold text-gray-500 hover:tracking-wide hover:text-slate-700 transition-all duration-500 delay-75 cursor-pointer">
-            Login
+            SignUp
           </p>
         </span>
         <form
@@ -28,16 +28,8 @@ const SignUp = () => {
           className="w-full h-full flex flex-col items-center justify-center gap-4"
         >
           <h1 className="font-bold text-5xl max-md:text-3xl text-slate-700">
-            Sign Up.
+            LogIn.
           </h1>
-          <input
-            type="text"
-            placeholder="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-1/2 input-field"
-            required
-          />
           <input
             type="text"
             placeholder="email"
@@ -55,18 +47,17 @@ const SignUp = () => {
             required
           />
           <button className="w-1/2 btn active-btn" type="submit">
-            Create Account
+            Log In
           </button>
           <p className="text-gray-400">OR</p>
           <button className="w-1/2 btn flex items-center justify-center bg-blue-600 text-white">
             Sign in With
-            <FaGoogle className="ml-2 " />
+            <FaGoogle className="ml-2" />
           </button>
         </form>
       </div>
-      <div className="h-screen w-screen sign-up-background max-md:hidden" />
     </div>
   );
 };
 
-export default SignUp;
+export default Login;
