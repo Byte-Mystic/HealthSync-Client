083fd2e7a99bf407ade7375ff04d2b0e6cb9cb01
Rishi060Mishra <110656861+Rishi060Mishra@users.noreply.github.com>
2024-04-30 01:27:43 +0530

    Report History modified in mammography

--- components/MedicalReportMammo/reportHistory.jsx
+++ components/MedicalReportMammo/reportHistory.jsx
@@ -39,19 +39,17 @@ const ReportHistory = ({ loading }) => {
         Report Results.
       </h1>
       {data?.map((item, index) => (
-        <div className="flex items-start justify-start gap-4" key={index}>
+        <div className="h-1/3 flex flex-col items-start justify-start gap-1" key={index}>
           <img
             src={item.image}
             alt=""
-            className="h-14 w-14 object-cover rounded-lg"
+            className="mb-2 h-3/4 w-full object-cover rounded-lg"
           />
-          <span className="flex flex-col justify-between">
-            <h1 className="font-semibold text-white text-xs">{item.user_mammo_id}</h1>
-            <span className="flex flex-col">
+            <span className="w-full flex items-center justify-between">
+            <h1 className="text-slate-400 text-xs">{item.user_mammo_id}</h1>
               <h1 className="text-slate-400 text-xs">{item.result}</h1>
-              <h1 className="text-slate-400 text-xs italic">{item.created_at}</h1>
             </span>
-          </span>
+              <h1 className="font-medium text-slate-400 text-xs italic" >{item.created_at}</h1>
         </div>
       ))}
     </div>
