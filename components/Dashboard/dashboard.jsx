7b51220b540477fb54ef2f64e81e6f49fa70a6eb
negikartik18 <negi18kartik@gmail.com>
2024-05-15 08:18:06 +0530

    user settings popup

--- components/Dashboard/dashboard.jsx
+++ components/Dashboard/dashboard.jsx
@@ -1,39 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../LeftSideBar/main";
 import ChatHistory from "../Chats/chatHistory";
 import { FaGripLines } from "react-icons/fa6";
+import { IoIosLogOut } from "react-icons/io";
 import Chat from "../Chats/chat";
-
-// const Dashboard = () => {
-//   return (
-//     // Layout
-//     <div className="h-screen w-screen flex bg-neutral-900">
-//       {/* Leftside bar */}
-//       <Main className="w-1/4" />
-
-//       {/* Middle Section */}
-//       <div className="w-3/4 my-6 flex rounded-2xl bg-neutral-800">
-//         {/* Chat Section */}
-//         <div className="w-full border-r border-r-neutral-700"></div>
-
-//         {/* Chat History & Logout */}
-//         <div className="w-96 flex flex-col">
-//           <div className="h-20 px-4 flex items-center justify-between border-b border-b-neutral-700">
-//             <img
-//               src="../../src/assets/bg-2.jpg"
-//               className="h-10 w-10 object-cover rounded-full"
-//               alt=""
-//             />
-//             <MdLogout className="text-xl text-neutral-200 transition ease-in-out duration-300 delay-75 hover:text-neutral-400 hover:scale-95 cursor-pointer" />
-//           </div>
-//           <ChatHistory />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+import UserSettings from "../UserSettings/userSettings";
 
 const Dashboard = () => {
+  const [visible, setVisible] = useState(false);
+
+  const togglePopup = () => {
+    setVisible(!visible);
+  };
+
   return (
     <div className="h-screen w-screen p-6 md:flex md:gap-4 max-md:p-0 bg-neutral-900">
       {/* LeftSideBar */}
@@ -46,13 +25,16 @@ const Dashboard = () => {
           <FaGripLines className="text-2xl text-gray-500" />
         </span>
         <Chat />
-        <div className="width-30 h-full py-6 flex flex-col border-l border-neutral-600 max-lg:hidden">
-          <div className="h-12 flex items-center justify-between">
-            {/* <img
+        <div className="width-30 h-full pb-6 flex flex-col border-l border-neutral-600 max-lg:hidden">
+          <div className="h-16 p-6 flex items-center justify-end gap-10 relative">
+            <img
               src="../../src/assets/bg-2.jpg"
               alt=""
-              className="h-10 w-10 rounded-full object-cover"
-            />*/}
+              className="h-8 w-8 rounded-full object-cover cursor-pointer"
+              onClick={togglePopup}
+            />
+            {visible && <UserSettings />}
+            <IoIosLogOut className="text-2xl text-white transition ease-in-out duration-300 cursor-pointer hover:scale-95 hover:text-gray-400" />
           </div>
           <ChatHistory />
         </div>
